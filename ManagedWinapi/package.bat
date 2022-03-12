261b40a9ddd23a76ee1f75bd48d22fe85c2192ba
Ben Hutchison <ben@aldaviva.com>
2022-03-12 13:57:31 -0800

    Multi-target .NET Framework 4.0 and .NET Core 3.1

--- ManagedWinapi/package.bat
+++ ManagedWinapi/package.bat
@@ -1,5 +1,7 @@
 @echo off
 
-dotnet clean -c ReleaseUnsigned ManagedWinapi-2010.csproj
-dotnet build -c ReleaseUnsigned ManagedWinapi-2010.csproj
-nuget pack -OutputDirectory .\bin\Release ManagedWinapi-2010.csproj.nuspec
+dotnet clean -c ReleaseUnsigned ManagedWinapi-2022.csproj
+dotnet clean -c ReleaseSigned   ManagedWinapi-2022.csproj
+
+dotnet pack -c ReleaseUnsigned --output .\bin\Release ManagedWinapi-2022.csproj
+dotnet pack -c ReleaseSigned   --output .\bin\ReleaseSigned ManagedWinapi-2022.csproj
